97b5b6d67fa149c0498f706e6063de3ec098f742
Alber Tenez <albert@zenettech.com>
2024-08-04 13:59:24 +0200

    Fixes style

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -6,7 +6,9 @@ export default function Contact(
 ) {
   return (
     <section class="w-full text-center p-4 rounded bg-primary">
-      <div class="mb-4 text-xl font-bold">{i18n.t('GET_IN_TOUCH')}</div>
+      <div class="mb-4 text-xl font-bold font-headings">
+        {i18n.t('GET_IN_TOUCH')}
+      </div>
       <div>
         <a
           class="text-black hover:text-secondary"
