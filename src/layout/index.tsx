6a2690e19775cb41268c4ec73bf0bcc69fdb6c44
Alber Tenez <albert@zenettech.com>
2024-08-04 13:52:32 +0200

    Reduce amount of loaded fonts

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -58,11 +58,7 @@ export default function Layout(
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#0b2027" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap"
           rel="stylesheet"
         />
         {hasQueryParams && (
